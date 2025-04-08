6a462e9865dad941839812f5116a61e0b9cb71a9
Felix Olawale <walefelix1998@gmail.com>
2025-04-08 16:38:56 +0100

    change from bcrypt to bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 const { isEmail, isStrongPassword } = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const userSchema = new Schema({
     email: {
         type: String,
